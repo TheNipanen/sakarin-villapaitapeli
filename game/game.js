afc41b80ab0d915d94fb1b226f2ebfec0a8953b0
Niilo Lappi <niilo.lappi@iki.fi>
2024-12-22 23:57:21 +0200

    Add audio

--- game/game.js
+++ game/game.js
@@ -1,4 +1,20 @@
 function changeScene(closeId, openId) {
   document.getElementById(closeId).classList.remove("active")
   document.getElementById(openId).classList.add("active")
+  playAudio(`${openId}-audio`)
+}
+
+function playAudio(id) {
+  const element = document.getElementById(id)
+  if (element && element.play) {
+    element.play()
+  }
+}
+
+function stopAudio(id) {
+  const element = document.getElementById(id)
+  if (element && element.pause) {
+    element.pause()
+    element.currentTime = 0
+  }
 }
